ee84a7d1280c360cada422305ff0fadcfdffc379
xi816 <jej07267@gmail.com>
2023-11-26 15:00:00 +0300

    Added strings and variables (examples/variables.gj)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GLE(J) -- a golfing programming language inspired by Jelly and APL
 
+## Usage
+To interpret a program, write `python3 path/to/your/program.gj`. Extension for **GLE(J)** programs is `.gj`
+
--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def run(code):
     stack = []
     retStack = []
     buf = ""
+    variables = {}
+    varNamesContain = "abcdefghijklmnopqrstuvwxyz"
 
     while (code[pos] != "\0"):
         ch = code[pos]
@@ -54,9 +56,50 @@ def run(code):
                 stack[-1] = sum(stack[-1])
         elif (ch == "i"):
             stack.append(int(input()))
+        elif (ch == "s"):
+            pos += 1
+            ch = code[pos]
+            if (ch == "i"):
+                stack.append(input())
         elif (ch == "+"):
             stack[-2] += stack[-1]
             stack.pop()
+        elif (ch == "_"):
+            stack[-2] -= stack[-1]
+            stack.pop()
+        elif (ch == "*"):
+            stack[-2] *= stack[-1]
+            stack.pop()
+        elif (ch == "/"):
+            stack[-2] /= stack[-1]
+            stack.pop()
+        elif (code[pos] == "\""):
+            pos += 1
+            while (code[pos] != "\""):
+                buf += code[pos]
+                pos += 1
+            stack.append(buf)
+            buf = ""
+        elif (ch == ":"):
+            pos += 1
+            while (code[pos] in varNamesContain):
+                buf += code[pos]
+                pos += 1
+            pos -= 1
+            variables[buf] = stack.pop()
+            buf = ""
+        elif (ch == "'"):
+            pos += 1
+            while (code[pos] in varNamesContain):
+                buf += code[pos]
+                pos += 1
+            pos -= 1
+            if (variables.get(buf) is not None):
+                stack.append(variables[buf])
+            else:
+                sys.stderr.write("Variable is not defined!")
+            buf = ""
+
         pos += 1
 
 assert sys.argv[1].endswith(".gj"), f"File extension `.{sys.argv[1].split('.')[-1]}` is not supported. The supported one is `.gj`"
